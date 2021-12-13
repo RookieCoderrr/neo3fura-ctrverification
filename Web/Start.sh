cce47deffd29dd464696b71cefa235c87384f581
RookieCoderrr <zilie.imtatlantique@gmail.com>
2021-12-13 16:00:59 +0800

    update local main

--- Web/Start.sh
+++ Web/Start.sh
@@ -4,27 +4,27 @@ echo you env is $1
 if [ $1 == "TEST" ]
 then
 
-    docker stop verifyContract
+    docker stop verifyContract_testnet
 
-    docker container rm verifyContract
+    docker container rm verifyContract_testnet
 
-    docker rmi verify:v1
+    docker rmi verify_testnet:v1
 
-    docker build -t verify:v1 .
+    docker build -t verify_testnet:v1 .
 
-    docker run --env RUNTIME="testnet" -itd --name verifyContract -p 3026:1927 verify:v1
+    docker run --env RUNTIME="testnet" -itd --name verifyContract_testnet -p 3026:1927 verify_testnet:v1
 fi
 
-if [ $1 == "MAIN" ]
+if [ $1 == "STAGING" ]
 then
 
-    docker stop verifyContract
+    docker stop verifyContract_mainnet
 
-    docker container rm verifyContract
+    docker container rm verifyContract_mainnet
 
-    docker rmi verify:v1
+    docker rmi verify_mainnet:v1
 
-    docker build -t verify:v1 .
+    docker build -t verify_mainnet:v1 .
 
-    docker run --env RUNTIME="mainnet" -itd --name verifyContract -p 3027:1927 verify:v1
+    docker run --env RUNTIME="mainnet" -itd --name verifyContract_mainnet -p 3027:1927 verify_mainnet:v1
 fi
