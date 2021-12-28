6d063061556ec18b11aa58f71ca29d5cf36e3d80
RookieCoderrr <zilie.imtatlantique@gmail.com>
2021-12-28 15:46:38 +0800

    add java compiler

---
+++ Web/buildgradle.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#cd /go/application/javaContractDemo
+
+sed -i "28c className ="$1"" build.gradle
+
+OPTS=""
+SLASH="/"
+var=$1
+var=${var//./ }  
+for element in $var
+do
+    OPTS="$OPTS$element$SLASH"
+done
+OPTS=${OPTS%?}
+echo $OPTS
+ClassName=${OPTS##*/}
+echo $ClassName
+
+Package=${OPTS%/*}
+echo $Package
+
+cd src/main/java
+
+rm -rf *
+
+mkdir -p $Package
+
+cd $Package
+
+#cp /go/application/$2/$ClassName go/appliaction/javaContractDemo/src/main/java/$Pacakge
+
+#cd /go/application/javaContractDemo
+
+#./gradlew neow3jCompile
